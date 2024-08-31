876360cd99ca384f1025d36ea7a13276e69e2fc2
Gian <catania.gian@gmail.com>
2024-08-31 12:24:51 -0300

    Second commit

--- script/GameManager.cs
+++ script/GameManager.cs
@@ -3,15 +3,17 @@ using Godot;
 public partial class GameManager : Node
 {
     [Export] private CharacterBody3D _rudder = null;
-    [Export] private Camera3D _firstScreen = null;
+    [Export] private WorldScreen _worldScreen = null;
 
     public override void _PhysicsProcess(double delta)
     {
+        _worldScreen.ApplyAccelerationCamera(delta);
         ApplyRotation();
     }
 
     private void ApplyRotation()
     {
-        _firstScreen.Rotation = new Vector3(_firstScreen.Rotation.X, _firstScreen.Rotation.Y, -_rudder.Rotation.Z);
+        _worldScreen.ApplyRotationCamera(-_rudder.Rotation.Z);
+        //_worldScreen.Rotation = new Vector3(_worldScreen.Rotation.X, _worldScreen.Rotation.Y, -_rudder.Rotation.Z);
     }
 }
